32043a6f8d66528fa63819ad2dcd69c753025bae
Daniel Worthington <danielworthington@gmail.com>
2015-06-09 15:55:50 -0700

    Added example of buttons acting as radio buttons

---
+++ styleguide/buttons/radio/example.js
@@ -0,0 +1,4 @@
+angular.module('brandai.example.button.radio', ['ui.bootstrap']);
+angular.module('brandai.example.button.radio').controller('ExampleButtonRadioController', function ($scope) {
+  $scope.radioModel = 'Middle';
+});
