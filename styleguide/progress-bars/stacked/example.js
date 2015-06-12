03ff23c4afe20c4a8ba141b176b0efa3c007022e
Daniel Worthington <danielworthington@gmail.com>
2015-06-11 23:40:14 -0700

    Added stacked progress bar example

---
+++ styleguide/progress-bars/stacked/example.js
@@ -0,0 +1,6 @@
+angular.module('brandai.example.progressbar', ['ui.bootstrap']);
+angular.module('brandai.example.progressbar').controller('ExampleStackedProgressbarController', function ($scope) {
+  $scope.max = 100;
+  $scope.bar1Percent = [[bar1Percent]];
+  $scope.bar2Percent = [[bar2Percent]];
+});
