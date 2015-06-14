07646348ebfef85995e1214295c35bed6a18961b
Daniel Worthington <danielworthington@gmail.com>
2015-06-13 21:49:09 -0700

    Added tooltip example

---
+++ styleguide/tooltip/tooltip/example.js
@@ -0,0 +1,4 @@
+angular.module('brandai.example.tooltip', ['ui.bootstrap']);
+angular.module('brandai.example.tooltip').controller('ExampleTooltipController', function ($scope) {
+  $scope.dynamicTooltipText = 'Dynamic tooltip text';
+});
